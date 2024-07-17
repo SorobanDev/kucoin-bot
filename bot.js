17a3fbe14782cf897ed40ee5fe50d2cb67cbb271
allaadev <hackcity2021@gmail.com>
2024-07-17 07:01:07 +0300

    bot

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ var ccxt = require("ccxt");
 var axios = require("axios");
 const http = require("http");
 require("dotenv").config();
-const exchanges = ccxt.exchanges;
+
 const kucoin = new ccxt.kucoinfutures({
   apiKey: process.env.KUCOIN_API_KEY,
   secret: process.env.KUCOIN_API_SECRET,
@@ -70,11 +70,8 @@ class PricesArray {
 const prices = new PricesArray(3);
 
 const symbol = "NOTUSDTM";
-const btcSymbol = "BTC/USDT";
 
 const BASE_URL = "https://api-futures.kucoin.com";
-
-const fundingUrl = `/api/v1/funding-rate/${symbol}/current`;
 
 async function fetchOrder() {
   const res = await axios.get(
@@ -83,32 +80,8 @@ async function fetchOrder() {
 
   const orders = res.data;
   const top20Bids = orders.data.bids.slice(0, 20);
-  const top20Asks = orders.data.asks.slice(0, 20);
-  const demand = top20Bids.reduce((sum, arr) => sum + arr[1] * arr[0], 0);
-  const suply = top20Asks.reduce((sum, arr) => sum + arr[1] * arr[0], 0);
-  const top2Bids = orders.data.bids.slice(0, 2);
-  const top2Asks = orders.data.asks.slice(0, 2);
-  const demand2 = top2Bids.reduce((sum, arr) => sum + arr[1] * arr[0], 0);
-  const suply2 = top2Asks.reduce((sum, arr) => sum + arr[1] * arr[0], 0);
-  const minSuplyPrice = top20Asks[0][0];
-  const bestDemandPrice = top20Bids[0][0];
-  const maxBidDemand = top20Bids.reduce(
-    (max, current) => (current[1] > max[1] ? current : max),
-    top20Bids[0]
-  );
 
   prices.addElement(top20Bids[0][0]);
-
-  const maxSuply = top20Asks.reduce(
-    (max, current) => (current[1] > max[1] ? current : max),
-    top20Asks[0]
-  );
-
-  // Filter the arrays based on the price condition (price < maxPrice)
-
-  const sellPrice = top20Bids[0][0];
-  const maxPriceSupply = top20Asks[0][0] + 0.00002;
-  // console.log("the prices array", prices.getArray());
 
   if (prices.getArray().length > 1) {
     if (prices.calculateRegressionSlope()[0] < -1000) {
@@ -128,7 +101,7 @@ async function fetchOrder() {
 
     if (
       prices.calculateRegressionSlope()[0] <
-        prices.calculateRegressionSlope()[1] / 1.6 &&
+        prices.calculateRegressionSlope()[1] / 1.5 &&
       balance > 0
     ) {
       await kucoin.createOrder(symbol, "market", "sell", 750, "", {
@@ -138,17 +111,7 @@ async function fetchOrder() {
       sumMax = 0;
     }
   }
-
-  // console.log(balance);
 }
-
-// async function fetchOrdersSequentially() {
-//   for (let i = 0; i < 15; i++) {
-//     await fetchOrder();
-//     // console.log(`Fetch order ${i + 1} completed.`);
-//     await new Promise((resolve) => setTimeout(resolve, 1000)); // Delay between each fetchOrder call
-//   }
-// }
 
 async function fetchOrdersSequentially() {
   while (true) {
